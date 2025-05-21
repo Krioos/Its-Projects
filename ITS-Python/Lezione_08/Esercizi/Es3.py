f683c95c907f5affd833e432b329e5775a36976e
Krioos <alessionatale2k@gmail.com>
2025-05-21 14:02:18 +0200

    changes

--- ITS-Python/Lezione_08/Esercizi/Es3.py
+++ ITS-Python/Lezione_08/Esercizi/Es3.py
@@ -35,6 +35,33 @@ class Member():
         lista = stringa.split(",")
         return cls(lista[0].strip(), lista[1].strip())
     
+class Library():
+    total_books = 0
+    def __init__(self):
+        self.books = list()
+        self.members = list()
+
+    def add_book(self, book)-> None:
+        self.books.append(book)
+        total_books += 1
+    
+    def remove_book(self, book)-> None:
+        self.books.remove(book)
+        total_book -= 1
+
+    def register_member(self, member)-> None:
+        self.members.append(member)
+
+    def lend_book(self, member, book)-> None:
+        if book in self.books:
+            member.borrow_book(book)
+        else:
+            print(f"Error no book called {book.title} in library")
+
+    def __str__(self)-> str:
+        return f"The librabry has {self.total_books} books which are {self.books}\nLibrabry members: {self.members}"
+    
+    
 book_str: str = "La Divina Commedia, D. Alighieri, 999000666"
 divina_commedia: Book = Book.from_string(book_str)
 
