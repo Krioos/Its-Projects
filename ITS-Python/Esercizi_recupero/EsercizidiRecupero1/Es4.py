c394d489543ebc95ed28609746b45542ac68cefa
Krioos <alessionatale2k@gmail.com>
2025-06-19 13:57:14 +0200

    changes

--- ITS-Python/Esercizi_recupero/EsercizidiRecupero1/Es4.py
+++ ITS-Python/Esercizi_recupero/EsercizidiRecupero1/Es4.py
@@ -5,8 +5,32 @@ def isDNA(stringa: str)-> bool:
             return False
     return True
 
+def comparisonDNA(dna1:str, dna2:str)-> None:
+    if isDNA(dna1) and isDNA(dna2):
+        max_len:int  = max(len(dna1), len(dna2))
+        for i in range(max_len, 0, -1):
+            if dna1[-i:] == dna2[:i]:
+               overlap = i
+               break
+        if overlap != 0:
+            print(f"{dna1}\n{' ' * (len(dna1) - overlap)}{dna2}")
+            print(f"La massima lunghezza di sovrapposizione è {overlap}")
+        else:
+            print("Le due seguenza di DNA non sono sovrapponibili")
+    else:
+        print("Una delle due seguenza non è corretta")
+
 s1= "TTGACCAGGTCA"
 s2= "AACCGGTTAA"
-s3 = "Casa"
 
-print(f"{isDNA(s1)}\n{isDNA(s2)}\n{isDNA(s3)}")
+comparisonDNA(s1,s2)
+
+s1= "GGTACCGTGA"
+s2= "CGTGAACCTT"
+
+comparisonDNA(s1, s2)
+
+s1= "AAGCTTACG"
+s2= "ACGTTCGA"
+
+comparisonDNA(s1, s2)
